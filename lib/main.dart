1bd84a155bdf175322f363a45047f4b821096740
240040040 <240040040@stikom-bali.ac.id>
2025-01-15 08:02:48 +0800

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Music App',
+      title: 'Music Gallery',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.blue,
